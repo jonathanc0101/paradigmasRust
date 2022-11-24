c9f08322a9b991d9df78e00d4ca518ed0a488403
JC <jonathancavia0101@gmail.com>
2022-11-24 17:24:09 -0300

    comments

--- factorial_paralelizado/src/main.rs
+++ factorial_paralelizado/src/main.rs
@@ -1,17 +1,39 @@
-use num::{BigUint, Zero, One};
-use std::mem::replace;
+use num::{BigUint, One}; //numeros de tamaño arbitrario
 
-use parallel_factorial::factorial;
+use parallel_factorial::factorial; //factorial paralelizado
 
 
-fn fac(n:usize,b: BigUint) -> BigUint {
-    if n==0 || n == 1{
-        b
-    }else{
-        fac(n-1, n * b)
+fn fac(n:usize) -> BigUint {
+    // if n==0 || n == 1{
+    //     b
+    // }else{
+    //     fac(n-1, n * b)
+    // }
+    
+    let mut t = BigUint::one();
+    for x in 1..n {
+        t*=x;
     }
+    t
 }
 
 fn main() {
-    println!("fac(500000) = {}", factorial(500000));
+
+    use std::time::Instant;
+
+    let now = Instant::now();
+    {
+        println!("fac(1000000) = {}", factorial(1000000));
+    }
+    let elapsed = now.elapsed();
+
+    let now_np = Instant::now();
+    {
+        println!("fac(100000) = {}", fac(100000));
+    }
+    let elapsed_np = now_np.elapsed();
+
+
+    println!("Factorial paralelizado de 1000000 en: {:.8?}", elapsed);
+    println!("Factorial no paralelizado de 100000 en: {:.8?}", elapsed_np);
 }
